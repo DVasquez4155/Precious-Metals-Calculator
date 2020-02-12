c2cf80cc41c9c359ea4c52aabcd01551ba91f25d
Daniel V <main@dvasquez4155.com>
2020-02-11 22:39:44 -0800

    Added a working script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,87 @@
-// //Example of how this API can be used
-// var worldTrade = new getResponse(
-//   "http://metals-api.com/api/latest?access_key=km649dqf4vjyvakzc59zanvo88f05pya0u58ksl56b9ljaobo9whf3flwh8yl6m0",
-//   "",
-//   ""
-// );
-// // var cat = new getResponse("https://aws.random.cat/meow", "", "")
-// // weather.done(
-// //     function (response){
-// //         console.log(response)
-// //     }
-// // )
-// // cat.done(
-// //     function (response){
-// //         console.log(response)
-// //     }
-// // )
-// worldTrade.done(function(response) {
-//   console.log(response);
-// });
-
-// //Response
-// function getResponse(url, apiKey, query) {
-//   this.url = url;
-//   this.apiKey = apiKey;
-//   this.query = query;
-
-//   this.queryURL = function() {
-//     return this.url + this.apiKey + this.query;
-//   };
-//   var ajax = $.ajax({
-//     url: this.queryURL(),
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response);
-//   });
-//   return ajax;
-// }
-
-//
-
-// var queryURL =
-//   "http://metals-api.com/api/latest?access_key=km649dqf4vjyvakzc59zanvo88f05pya0u58ksl56b9ljaobo9whf3flwh8yl6m0";
-// //"&base=USD"+
-// //  "&symbols=XAU,XAG";
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response);
-// });
-
-// var queryURL =
-
-// http://metals-api.com/api/latest?access_key=km649dqf4vjyvakzc59zanvo88f05pya0u58ksl56b9ljaobo9whf3flwh8yl6m0
-
-// set endpoint and your access key
-endpoint = "latest";
-access_key = "km649dqf4vjyvakzc59zanvo88f05pya0u58ksl56b9ljaobo9whf3flwh8yl6m0";
-
-// get the most recent exchange rates via the "latest" endpoint:
-$.ajax({
-  url: "http://metals-api.com/api/" + endpoint + "?access_key=" + access_key,
-  dataType: "jsonp",
-  success: function(json) {
-    // exchange rata data is stored in json.rates
-    alert(json.rates.GBP);
-
-    // base currency is stored in json.base
-    alert(json.base);
-
-    // timestamp can be accessed in json.timestamp
-    alert(json.timestamp);
-  }
+var liveRates = new JSON({
+    url: "https://www.live-rates.com/rates",
+    api: "?key=" + "bc6cdd4bb0",
+    result: function (response) {
+        $(response).each(
+            function (data) {
+                var price = formatCurrency(response[data].rate);
+                var timeSince = moment(parseInt(response[data].timestamp)).calendar();
+                if (response[data].currency == "GOLD") {
+                    goldText.html(price);
+                    goldTimeText.html(timeSince)
+                } else if (response[data].currency == "SILVER") {
+                    silverText.html(price);
+                    silverTimeText.html(timeSince)
+                } else if (response[data].currency == "PLATINUM") {
+                    platinumText.html(price);
+                    platinumTimeText.html(timeSince)
+                } else if (response[data].currency == "PALLADIUM") {
+                    palladiumText.html(price);
+                    palladiumTimeText.html(timeSince)
+                }
+            }
+        )
+    }
 });
+
+// var alphavantage = new JSON(
+//         {
+//             url : "https://finnhub.io/api/v1/forex/candle",
+//             api : "?token=" + "bovg3ufrh5r90eafk9s0",
+//             query : "&symbol=OANDA:XAU_USD&resolution=5" + "&from="  + moment().subtract(10, 'minute').format('x').substring(0,10) + "&to="  + moment().format('x').substring(0,10),
+//             cors : true,
+//             result : function (response) {
+//                 $(response).each(function(i){
+//                      console.log(moment(parseInt(response.t[i] + "000")))
+//                 })
+//                 console.log(response)
+//             }
+//         }
+//     )
+
+// var alphavantage = new JSON(
+//     {
+//         url : "https://api.worldtradingdata.com/api/v1/forex",
+//         api : "?api_token=" + "zDjkfomCS01jLBkzW2EFz5KT2Bod85ci3WoQMVWBhKnLUfeLkOWTmG0jKAwR",
+//         query : "&base=USD",
+//         cors : true,
+//         result : function (response) {
+//             console.log(response)
+//         }
+//     }
+// )
+
+// var alphavantage = new JSON(
+//     {
+//         url : "https://www.alphavantage.co/query?function=SYMBOL_SEARCH",
+//         api : "&apikey=" + "VI73VEG5MXIDIMDZ",
+//         query : "&keywords=gold",
+//         cors : true,
+//         result : function (response) {
+//             console.log(response)
+//         }
+//     }
+// )
+
+// var intrinio = new JSON(
+//     {
+//         url : "https://api-v2.intrinio.com/securities/search",
+//         api : "?api_key=" + "OmUwMzQ3YzEyYWQ1YWQ0OWIxN2NlMjYyZDM0ZTRkYWY3",
+//         query : "&query=apple",
+//         cors : true,
+//         result : function (response) {
+//             console.log(response)
+//         }
+//     }
+// )
+
+// var metalsApi = new JSON(
+//     {
+//         url : "http://metals-api.com/api/latest",
+//         api : "?access_key=" + "6er5tg54g49w64i6e1lkp9ka7mpc5xwk1dv22v650i7022x2q47eih19pyfu64vw",
+//         cors : true,
+//         result : function (response) {
+//             console.log(response)
+//         }
+//     }
+// )
