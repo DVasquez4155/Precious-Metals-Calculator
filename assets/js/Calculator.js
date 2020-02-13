907b8b934d4040c7e4a9aef6bd5e47cec987ca21
Daniel V <main@dvasquez4155.com>
2020-02-12 18:20:28 -0800

    Update Calculator.js
    Changed onclick to onchange

--- assets/js/Calculator.js
+++ assets/js/Calculator.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  $('input[name="sub"]').on("click", function() {
+  $('input').on("change", function() {
     figureThis();
   });
+  figureThis();
 });
 
 function figureThis() {
